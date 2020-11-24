2c43bbafdc25e610c94b47fd870c8057ae4a631e
Mariela Gonzalez <mariela21180@gmail.com>
2020-11-24 20:48:43 -0300

    Java - Introduccion

---
+++ Java/1_Introduccion/Main.java
@@ -0,0 +1,69 @@
+class Main {
+  public static void main(String[] args) {
+
+    // // Variables numericas
+    // byte nByte = 10;
+    // short nShort = 10000;
+    // int nInt = 127;
+    // long nLong = 378461321;
+    // float nFloat = 45.64158f;
+    // double nDouble = 87.1654213;
+    // System.out.println(nByte);
+    // System.out.println(nShort);
+    // System.out.println(nInt);
+    // System.out.println(nLong);
+    // System.out.println(nFloat);
+    // System.out.println(nDouble);
+    // System.out.println(' ');
+
+    // // Variables Booleanas
+    // boolean vOf = true;
+    // System.out.println(vOf);
+    // System.out.println(' ');
+
+    // // Variables de texto
+    // char letra = 'h';
+    // String texto = "Hola Mundo!!";
+    // System.out.println(letra);
+    // System.out.println(texto);
+    // System.out.println(' ');
+
+    // // Arreglos    
+    // String marcaAuto[] = new String[5];
+    // marcaAuto[0] = "Chevrolet";
+    // marcaAuto[1] = "Fiat";
+    // marcaAuto[2] = "Peugeot";
+    // marcaAuto[3] = "Ford";
+    // marcaAuto[4] = "Renault";
+    // System.out.println(marcaAuto[0]);
+    // System.out.println(marcaAuto[1]);
+    // System.out.println(marcaAuto[2]);
+    // System.out.println(marcaAuto[3]);
+    // System.out.println(marcaAuto[4]);
+    // System.out.println(' ');
+
+    // // Condicionales
+    // int edad = 18;
+    // String texto = "hola";
+    // if(edad == 18) {
+    //     System.out.println("Tienes 18 años");
+    // } else {
+    //     System.out.println("No tienes 18 años");
+    // }
+    // if(edad >= 18) {
+    //     System.out.println("Puedes conducir un vehículo");
+    // } else {
+    //     System.out.println("No puedes conducir un vehículo");
+    // }
+    // if(texto == "hola") {
+    //     System.out.println("¿Como estás?");
+    // }
+
+    // Matrices
+    String tableroDamas[][] = new String[8][8];
+    tableroDamas[0][2] = "Blanca";
+    tableroDamas[6][2] = "Roja";
+    System.out.println(tableroDamas[0][2]);
+    System.out.println(tableroDamas[6][2]);
+  }
+}

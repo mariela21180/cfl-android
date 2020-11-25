06878f4650ad98757bf0095f766dca753a92da20
Mariela Gonzalez <mariela21180@gmail.com>
2020-11-24 21:33:11 -0300

    Java - Condicionales e Iteraciones

---
+++ Java/1_Introduccion/CondicionalesIteraciones.java
@@ -0,0 +1,116 @@
+class CondicionalesIteraciones {
+    
+  public static void main(String[] args) {
+
+    // // 1) Condicionales anidados
+    // int edad = 18;
+    // char sexo = 'f';
+    // if(edad >= 21 || sexo =='m') {
+    //     System.out.println("Corresponde remera azul");
+    // } else if (sexo == 'f'){
+    //     System.out.println("Corresponde remera roja");
+    // }
+
+
+    // // 2) Operadores Aritméticos
+    // int resultado, numero;
+    // // Operaciones
+    // resultado = 3 + 6;
+    // resultado = 5 - 3;
+    // resultado = 2 * 4;
+    // resultado = 8 / 6;
+    // resultado = 9 % 2;
+    // System.out.println(resultado);
+
+    // numero = 1;
+    // numero = numero + 5;
+
+    // // Autooperaciones
+    // numero += 5;
+    // numero -= 3;
+    // numero *= 2;
+    // numero /= 3;
+    // System.out.println(numero);
+
+    // // Incremento y decremento
+    // numero = 1;
+    // numero++; //numero = numero + 1;
+    // numero--; //numero = numero - 1;
+    // resultado = numero++; // Asigno y luego incremento
+    // resultado = ++numero; // Incremento y luego asigno
+
+    // System.out.println("El Resultado es: " + resultado);
+    // System.out.println("El Numero es: " + numero);
+
+
+
+    // // 3) Condicional Switch
+    // int mes = 15;
+    // switch(mes){
+    //   case 1: 
+    //   case 3: 
+    //   case 5: 
+    //   case 7: 
+    //   case 8: 
+    //   case 10: 
+    //   case 12: System.out.println("El mes " + mes + " tiene 31 días");
+    //     break;
+    //   case 4: 
+    //   case 6: 
+    //   case 9: 
+    //   case 11: System.out.println("El mes " + mes + " tiene 30 días");
+    //     break;
+    //   case 2: System.out.println("El mes " + mes + " tiene 28 días");
+    //     break;
+    //   default: System.out.println("El mes " + mes + " no existe");
+    // }
+
+    // // 4) Iteraciones
+    // int i = 0;
+    // while(i <= 4) {
+    //   System.out.println("Ciclo While: " + i);
+    //   i++;
+    // }
+    // System.out.println(" ");
+
+    // i = 0;
+    // do{
+    //   System.out.println("Ciclo DoWhile: " + i);
+    //   i++;
+    // } while(i <= 4);
+    // System.out.println(" ");
+
+    // for(int j = 0; j <= 4; j++){
+    //   System.out.println("Ciclo for: " + j);
+    // }
+
+    // // 5) Iteraciones Anidadas 
+    // System.out.println("Bucle FOR anidado\n");        
+    // for(int i = 1; i <= 10; i++){
+    //   for (int j = 1; j <=10; j++) {
+    //     System.out.print(i + " * " + j + " = " + i*j + " | ");        
+    //   }
+    //   System.out.println("\n");        
+    // }
+
+    // System.out.println("Recorrido Matriz\n");        
+    // int matriz[][] = new int[10][10];
+    // for(int i = 0; i <= 9; i++){
+    //   for (int j = 0; j <=9; j++) {
+    //     matriz[i][j] = i*j;
+    //     System.out.print(i + " * " + j + " = " + matriz[i][j] + " | ");        
+    //   }
+    //   System.out.println("\n");        
+    // }
+
+    // // 6) Matrices    
+    // String tableroDamas[][] = new String[8][8];
+    // tableroDamas[0][2] = "Blanca";
+    // tableroDamas[6][2] = "Roja";
+    // System.out.println(tableroDamas[0][2]);
+    // System.out.println(tableroDamas[6][2]);
+
+
+  }
+    
+}
